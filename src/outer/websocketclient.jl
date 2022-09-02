f2f74f823eb419fe49f362c0d7f4dbb8cf26696c
Apáti-Nagy Attila <104444676+apati-nagy-attila-ardinsys@users.noreply.github.com>
2022-09-02 11:24:23 +0200

    Change websocket event handling (#27)
    * Fixing broken websocket tests
    
    * Add Token to WebSocketOpen and OpenEvent
    
    * Get rid fo WebSocketReceive and WebSocketEvent. It will be better to create simple data structs and use them
    
    * Expand error handling
    
    * Fix identation
    
    * Review fix.
    
    Co-authored-by: Apáti-Nagy Attila <apati-nagy_attila@ardinsys.eu>

--- src/outer/websocketclient.jl
+++ src/outer/websocketclient.jl
@@ -2,8 +2,9 @@
 module WebsocketClient
 
 export WebSocketClient
-export WebSocketEvent, CloseEvent, MessageEvent, ErrorEvent, OpenEvent
-export WebSocketCallerActor, WebSocketMessage, WebSocketClose, WebSocketOpen, WebSocketSend, WebSocketReceive
+export WebSocketCallerActor
+export WebSocketMessage, WebSocketClose, WebSocketOpen, WebSocketSend
+export CloseEvent, MessageEvent, ErrorEvent, OpenEvent
 
 using HTTP, HTTP.WebSockets
 using Sockets
@@ -36,24 +37,48 @@ struct WebSocketClose <: WebSocketMessage
 end
 
 struct WebSocketOpen <: WebSocketMessage
+    token::Token
     source              #sender Actor Addr
-    url
+    url                 #should contain protocol and port if needed
+
+    WebSocketOpen(source, url) = new(Token(), source, url)
 end
 
-abstract type WebSocketEvent end
-struct CloseEvent <: WebSocketEvent end
-struct MessageEvent <: WebSocketEvent end
-struct ErrorEvent <: WebSocketEvent end
-struct OpenEvent <: WebSocketEvent end
+struct CloseEvent
+    websocketid::UInt32
+    status::Int
+    response::AbstractVector{UInt8}         # message
 
-struct WebSocketReceive
-    type::WebSocketEvent
+    CloseEvent(websocketId, status, response) = new(websocketId, 1000, response)
+    CloseEvent(websocketId, status, response::String) = new(websocketId, status,  Vector{UInt8}(response))
+end
+
+struct MessageEvent
     websocketid::UInt32
     response::AbstractVector{UInt8}
 
-    WebSocketReceive(type, websocketId, response) = new(type, websocketId, response)
-    WebSocketReceive(type, websocketId, response::String) = new(type, websocketId, Vector{UInt8}(response))
+    MessageEvent(websocketId, response) = new(websocketId, response)
+    MessageEvent(websocketId, response::String) = new(websocketId, Vector{UInt8}(response))
 end
+
+struct ErrorEvent
+    websocketid::UInt32
+    response::AbstractVector{UInt8}
+
+    ErrorEvent(websocketId, response) = new(websocketId, response)
+    ErrorEvent(websocketId, response::String) = new(websocketId, Vector{UInt8}(response))
+end
+
+struct OpenEvent
+    token::Token
+    websocketid::UInt32
+    response::AbstractVector{UInt8}
+
+    OpenEvent(token, websocketId, response) = new(token, websocketId, response)
+    OpenEvent(token, websocketId, response::String) = new(token, websocketId, Vector{UInt8}(response))
+end
+responsetype(::Type{<:WebSocketOpen}) = OpenEvent
+
 
 websocket_id(msg::WebSocketOpen) = missing
 websocket_id(msg::WebSocketSend) = UInt64(msg.websocketid)
@@ -91,28 +116,43 @@ end
 
 function Circo.onmessage(me::WebSocketCallerActor, openmsg::WebSocketOpen, service)    
 
-    @async WebSockets.open("ws://$(openmsg.url)"; verbose=false) do ws
+    connectiontask = @async WebSockets.open("$(openmsg.url)"; verbose=false) do ws
         websocket_id = rand(UInt32)
         get!(me.messageChannels, websocket_id, ws)
         
         @debug "Client Websocket connection established!"
-        Circo.send(service, me, openmsg.source, WebSocketReceive(OpenEvent(), websocket_id, "Websocket connection established!"))
+        Circo.send(service, me, openmsg.source, OpenEvent(openmsg.token, websocket_id, "Websocket connection established!"))
 
+        closeeventsent = false
         try 
             for raw_message in ws
                 @debug "Client got from server rawMessage" String(raw_message)
-                Circo.send(service, me, openmsg.source, WebSocketReceive(MessageEvent(), websocket_id, raw_message))
+                Circo.send(service, me, openmsg.source, MessageEvent(websocket_id, raw_message))
             end
         catch e
-            if !(e isa EOFError)
-                @info "Exception in arrivals", e
+            @warn "Exception" e
+            if typeof(e) == HTTP.WebSockets.WebSocketError
+                if typeof(e.message) == HTTP.WebSockets.CloseFrameBody
+                    Circo.send(service, me, openmsg.source, CloseEvent(websocket_id, e.message.status, e.message.message))
+                    closeeventsent = true
+                else
+                    Circo.send(service, me, openmsg.source, ErrorEvent(websocket_id, e.message))
+                end
             end
         finally
-            Circo.send(service, me, openmsg.source, WebSocketReceive(CloseEvent(), websocket_id, "Websocket connection closed"))
+            if !closeeventsent
+                Circo.send(service, me, openmsg.source, CloseEvent(websocket_id, 1000, "Websocket connection closed"))
+            end
             delete!(me.messageChannels, websocket_id)
         end
         # unnecessary
         # close(ws)
+    end
+
+    if istaskfailed(connectiontask)
+        websocket_errorcode = connectiontask.result.error.message.status
+        @warn "Error occured when establishing websocket connection! Errorcode : $(websocket_errorcode)"
+        Circo.send(service, me, openmsg.source, ErrorEvent(websocket_id, websocket_errorcode))
     end
 end 
 
@@ -123,8 +163,11 @@ sending_websocket_message(ws, wsmessage::WebSocketSend) = HTTP.send(ws, wsmessag
 
 function Circo.onmessage(me::WebSocketCallerActor, wsmessage::WebSocketMessage, service)
     ws = get(me.messageChannels, websocket_id(wsmessage), missing)
-    # TODO handle missing
-    sending_websocket_message(ws, wsmessage)
+    if ws === missing
+        Circo.send(service, me, openmsg.source, ErrorEvent(websocket_id, "Got unknown websocket id"))
+    else 
+        sending_websocket_message(ws, wsmessage)
+    end
 end
 
 
--- test/http/websockettest.jl
+++ test/http/websockettest.jl
@@ -4,6 +4,9 @@ using Sockets
 using Circo, Circo.WebsocketClient
 using Logging
 
+include("../helper/testactors.jl");
+import .TestActors: Puppet, msgcount, msgs
+
 import Circo.send
 
 struct StartTestMsg
@@ -15,11 +18,12 @@ mutable struct WebSocketTestActor <: Actor{Any}
     core
     receivedmessages::AbstractArray
     expectedmessages::AbstractArray
+    errorevent::Vector
     websocketcaller
     websocketid::UInt32
     message
 
-    WebSocketTestActor(core, expectedmessages) = new(core, [], expectedmessages)
+    WebSocketTestActor(core, expectedmessages) = new(core, [], expectedmessages, [])
 end
 
 function Circo.onmessage(me::WebSocketTestActor, msg::StartTestMsg, service)
@@ -30,13 +34,10 @@ function Circo.onmessage(me::WebSocketTestActor, msg::StartTestMsg, service)
     send(service, me, me.websocketcaller, openmsg)
 end
 
-function Circo.onmessage(me::WebSocketTestActor, msg::WebSocketReceive, service)
-    @debug "Circo.onmessage(me::WebSocketTestActor, msg::WebSocketReceive, service)" msg.response
-    push!(me.receivedmessages, msg)
-    processmessage(me, msg, msg.type, service)
-end
+function Circo.onmessage(me::WebSocketTestActor, response::OpenEvent, service)
+    @info "Circo.onmessage openEvent" response
+    push!(me.receivedmessages, response)
 
-function processmessage(me::WebSocketTestActor, response::WebSocketReceive, ::OpenEvent, service)
     me.websocketid = UInt64(response.websocketid)
 
     sendmsg = WebSocketSend(me.message, addr(me), missing, me.websocketid)
@@ -44,24 +45,34 @@ function processmessage(me::WebSocketTestActor, response::WebSocketReceive, ::Op
     # NOTE Sending close messsage right after sending WebSocketSend may cause scheduling problem. It caused the WebSocketCallerActor read messages full of "/0" 
 end
 
-function processmessage(me::WebSocketTestActor, ::WebSocketReceive, ::MessageEvent, service)
+function Circo.onmessage(me::WebSocketTestActor, response::MessageEvent, service)
     @debug "Cliens sending close"
+    push!(me.receivedmessages, response)
+
     closeMsg = WebSocketClose(addr(me), me.websocketid)
     send(service, me, me.websocketcaller, closeMsg)
 end
 
-function processmessage(me::WebSocketTestActor, ::WebSocketReceive, ::CloseEvent, service)
+function Circo.onmessage(me::WebSocketTestActor, response::CloseEvent, service)
+    @info "Circo.onmessage(me::WebSocketTestActor, ::CloseEvent, service)"
+    push!(me.receivedmessages, response)
+
     @test size(me.receivedmessages) == size(me.expectedmessages)
 
     @debug "Client received messages" me.receivedmessages
-    @debug "Client received messages" map(x -> String(x.response), me.receivedmessages)
+    @debug "Client received messages" map(x -> String(copy(x.response)), me.receivedmessages)
     @debug "Client expected messages" me.expectedmessages
 
-    for i in 1:size(me.receivedmessages, 1)
+    for i in eachindex(me.receivedmessages)
         @test String(me.receivedmessages[i].response) == me.expectedmessages[i]
     end
 
     die(service, me; exit = true)
+end
+
+function Circo.onmessage(me::WebSocketTestActor, response::ErrorEvent, service)
+    @info "we got ErrorEvent" response
+    push!(me.errorevent, response)
 end
 
 mutable struct VerificationData
@@ -92,24 +103,33 @@ end
 function create_websocket_server(testserver::TestServer, url, port)
     @async WebSockets.listen(url, port; server=testserver.tcpserver, verbose=true) do ws
         @test ws.request isa HTTP.Request
-        for msg in ws
-            data = String(msg)
-            responsemessage = "Server send this : $data"
-            @debug responsemessage
-            HTTP.send(ws, responsemessage)
+        try
+            for msg in ws
+                data = String(msg)
+                
+                responsemessage = "Server send this : $data"
+                @debug responsemessage
+                testserver.server_verification_data.messagereceived = true
+                push!(testserver.server_verification_data.receivedmessages, data)
 
-            testserver.server_verification_data.messagereceived = true
-            push!(testserver.server_verification_data.receivedmessages, data)
+                if data == "Send error"
+                    @info "Sending \"Unexpected\" error"
+                    error(responsemessage)
+                else 
+                    HTTP.send(ws, responsemessage)
+                end
+            end
+        finally
+            @debug "Server start to close"
+            testserver.server_verification_data.websocketservercloses = true
+            testserver.wait_for_server_close = true
         end
-        @debug "Server start to close"
-        testserver.server_verification_data.websocketservercloses = true
-        testserver.wait_for_server_close = true
     end
 end
 
 function serverside_verification(testServer::TestServer, expectedMessages)
     actual = testServer.server_verification_data
-    @test timedwait(() -> testServer.wait_for_server_close == true, 10.0) === :ok
+    @test timedwait(() -> testServer.wait_for_server_close == true, 5.0) === :ok
 
     @test actual.websocketservercloses == true
     @test actual.messagereceived == true
@@ -137,17 +157,19 @@ end
 
 @testset "WebSockets" begin
     msgdata = "Random message"
+    protocol = "ws://"
     url = "127.0.0.1"
 
     @testset "Testing sending message not from f(ws)" begin
+        @info "Testing sending message not from f(ws)"
         port = UInt16(8086)
 
         testserver = create_websocket_server(url, port)
+        try 
+            messagechannel = Channel{}(2)
+            closingsignal = Channel{}(2)
 
-        messagechannel = Channel{}(2)
-        closingsignal = Channel{}(2)
-
-        @async WebSockets.open("ws://$(url):$(port)"; verbose=true) do ws
+            @async WebSockets.open("$(protocol)$(url):$(port)"; verbose=true) do ws
             HTTP.send(ws, "Client send message!")
             msg = HTTP.receive(ws)
             @debug "Client side", String(msg)
@@ -171,75 +193,152 @@ end
             "Client send message!", "Client : $(msgdata)", "Client : $(msgdata)"
         ])
 
+        finally
         close_with_test(testserver)
     end
+    end
 
-    # @testset "Testing with Circo actor" begin
-    #     port = UInt16(8086)
+    @testset "Testing with Circo actor" begin
+        @info "Testing with Circo actor"
+        port = UInt16(8086)
 
-    #     testserver = create_websocket_server(url, port)
+        testserver = create_websocket_server(url, port)
+        try
+            ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
+            testactor = WebSocketTestActor(emptycore(ctx), [
+                    "Websocket connection established!", "Server send this : $(msgdata)", "Websocket connection closed"
+                ]
+            )
 
-    #     ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
-    #     testactor = WebSocketTestActor(emptycore(ctx), [
-    #             "Websocket connection established!", "Server send this : $(msgdata)", "Websocket connection closed"
-    #         ]
-    #     )
+            scheduler = Scheduler(ctx, [testactor])
+            scheduler(;remote=false)
 
-    #     scheduler = Scheduler(ctx, [testactor])
-    #     scheduler(;remote=false) # to spawn the zygote
+            send(scheduler, testactor, StartTestMsg(msgdata, "$(protocol)$(url):$(port)"))
+            scheduler(;remote=true)
 
-    #     send(scheduler, testactor, StartTestMsg(msgdata, "$(url):$(port)"))
+            @test findfirst(x -> typeof(x) == CloseEvent && x.status == 1000, testactor.receivedmessages) !== nothing
+            @test isempty(testactor.errorevent)
 
-    #     scheduler(;remote=true)
+            serverside_verification(testserver, [
+                msgdata
+            ])
 
-    #     serverside_verification(testserver, [
-    #         msgdata
-    #     ])
+            verify_websocket_clientactor(scheduler)
+            Circo.shutdown!(scheduler)
+        finally
+            close_with_test(testserver)
+        end
 
-    #     verify_websocket_clientactor(scheduler)
+    end
 
-    #     close_with_test(testserver)
-    #     Circo.shutdown!(scheduler)
-    # end
+    @testset "Testing with multiple connection" begin
+        @info "Testing with multiple connection"
+        portOne = UInt16(8086)
+        portTwo = UInt16(8087)
 
-    # @testset "Testing with multiple connection" begin
-    #     portOne = UInt16(8086)
-    #     portTwo = UInt16(8087)
+        testServerOne = create_websocket_server(url, portOne)
+        testServerTwo = create_websocket_server(url, portTwo)
+        try
+            msgdataTwo = "Different message"
 
-    #     testServerOne = create_websocket_server(url, portOne)
-    #     testServerTwo = create_websocket_server(url, portTwo)
+            ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
+            testActorOne = WebSocketTestActor(emptycore(ctx), [
+                    "Websocket connection established!", "Server send this : $(msgdata)", "Websocket connection closed"
+                ]
+            )
+            testActorTwo = WebSocketTestActor(emptycore(ctx), [
+                    "Websocket connection established!", "Server send this : $(msgdataTwo)", "Websocket connection closed"
+                ]
+            )
 
-    #     msgdataTwo = "Different message"
+            scheduler = Scheduler(ctx, [testActorOne, testActorTwo])
+            scheduler(;remote=false) # to spawn the zygote
 
-    #     ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
-    #     testActorOne = WebSocketTestActor(emptycore(ctx), [
-    #             "Websocket connection established!", "Server send this : $(msgdata)", "Websocket connection closed"
-    #         ]
-    #     )
-    #     testActorTwo = WebSocketTestActor(emptycore(ctx), [
-    #             "Websocket connection established!", "Server send this : $(msgdataTwo)", "Websocket connection closed"
-    #         ]
-    #     )
+            send(scheduler, testActorOne, StartTestMsg(msgdata,  "$(protocol)$(url):$(portOne)"))
+            send(scheduler, testActorTwo, StartTestMsg(msgdataTwo,  "$(protocol)$(url):$(portTwo)"))
 
-    #     scheduler = Scheduler(ctx, [testActorOne, testActorTwo])
-    #     scheduler(;remote=false) # to spawn the zygote
+            scheduler(;remote=true)
 
-    #     send(scheduler, testActorOne, StartTestMsg(msgdata, "$(url):$(portOne)"))
-    #     send(scheduler, testActorTwo, StartTestMsg(msgdataTwo, "$(url):$(portTwo)"))
+            @test findfirst(x -> typeof(x) == CloseEvent && x.status == 1000, testActorOne.receivedmessages) !== nothing
+            @test isempty(testActorOne.errorevent)
 
-    #     scheduler(;remote=true)
+            @test findfirst(x -> typeof(x) == CloseEvent && x.status == 1000, testActorTwo.receivedmessages) !== nothing
+            @test isempty(testActorTwo.errorevent)
 
-    #     serverside_verification(testServerOne, [
-    #         msgdata
-    #     ])
-    #     serverside_verification(testServerTwo, [
-    #         msgdataTwo
-    #     ])
 
-    #     verify_websocket_clientactor(scheduler)
+            serverside_verification(testServerOne, [
+                msgdata
+            ])
+            serverside_verification(testServerTwo, [
+                msgdataTwo
+            ])
 
-    #     close_with_test(testServerOne)
-    #     close_with_test(testServerTwo)
-    #     Circo.shutdown!(scheduler)
-    # end
+            verify_websocket_clientactor(scheduler)
+            Circo.shutdown!(scheduler)
+        finally   
+          close_with_test(testServerOne)
+          close_with_test(testServerTwo)
+        end
+    end
+
+    @testset "Testing simple error() ErrorEvent" begin
+        @info "Testing simple error() ErrorEvent"
+        port = UInt16(8086)
+        errormsg = "Send error"
+        testserver = create_websocket_server(url, port)
+        try
+            ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
+            testactor = WebSocketTestActor(emptycore(ctx), [
+                "Websocket connection established!",
+                "Unexpected server websocket error"
+                ])
+
+            scheduler = Scheduler(ctx, [testactor])
+            scheduler(;remote=false)
+
+            send(scheduler, testactor, StartTestMsg(errormsg, "$(protocol)$(url):$(port)"))
+            scheduler(;remote=true)
+
+            @test findfirst(x -> typeof(x) == CloseEvent && x.status > 1000, testactor.receivedmessages) !== nothing
+            @test isempty(testactor.errorevent)
+
+            serverside_verification(testserver, [errormsg])
+
+            verify_websocket_clientactor(scheduler)
+            Circo.shutdown!(scheduler)
+        finally
+            close_with_test(testserver)
+        end
+        
+    end
+
+    @testset "Testing with Circo actor" begin
+        @info "Testing with Circo actor"
+        port = UInt16(8086)
+
+        testserver = create_websocket_server(url, port)
+        try
+            ctx = CircoContext(target_module=@__MODULE__, userpluginsfn=() -> [WebSocketClient])
+            puppet = TestActors.Puppet()
+            scheduler = Scheduler(ctx, [puppet])
+            scheduler(;remote=false)
+
+            send(scheduler, puppet, StartTestMsg(msgdata, "$(protocol)$(url):$(port)"))
+            @async scheduler(;remote=true)
+
+            websocketcalleraddr = getname(scheduler.service, "websocketclient")
+            websocketcaller = getactorbyid(scheduler, websocketcalleraddr.box)
+
+            send(scheduler, puppet, WebSocketSend("Something unrelevant", addr(puppet), missing, rand(UInt32)))
+            
+            errorevent = get(puppet.msgs, ErrorEvent, missing)
+            @test errorevent === missing
+
+
+            Circo.shutdown!(scheduler)
+        finally
+            close_with_test(testserver)
+        end
+
+    end
 end
